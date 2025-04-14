e21c3ef6b35ab1412301b93fb5382a635c75aa0b
BharathSunkarKumar <155315751+BharathSunkaraKumar@users.noreply.github.com>
2025-04-14 16:04:28 +0530

    3js

--- react/reactApps/React-Apps1/src/App.jsx
+++ react/reactApps/React-Apps1/src/App.jsx
@@ -1,27 +1,10 @@
-import { useState } from 'react'
 
 import './App.css'
-import User from './useReducer/User'
-import TodoList from './useReducer/TodoList'
 import StudentForm from './formik/StudentForm'
-// import Component1 from './contex/Component1'
-// import Darkmode from './darkMode/Darkmode'
-// import Todo from './todoApp/Todo'
-// import Cocktail from './cocktail/Cocktail'
-// import Review from './reviewapp/Review'
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <div>
-        {/* <Todo/> */}
-        {/* <Cocktail/> */}
-        {/* <Review/> */}
-        {/* <Darkmode/> */}
-        {/* <Component1/> */}
-        {/* <User/> */}
-        {/* <TodoList/> */}
         <StudentForm/>
       </div>
     </>
--- react/reactApps/React-Apps1/src/formik/StudentForm.jsx
+++ react/reactApps/React-Apps1/src/formik/StudentForm.jsx
@@ -2,31 +2,65 @@ import React, { useState } from 'react'
 import {useFormik} from 'formik'
 import * as Yup from 'yup'
 import './Form.css'
+import axios from 'axios'
 function StudentForm() {
     const [students, setStudents] = useState([])
     const Studentform = useFormik({
         initialValues : {
             firstname: '',
             lastname: '',
-            age: '',     
+            gender: '',
+            age: '',   
+            username: ''  
         },
         validationSchema:Yup.object({
-            firstname: Yup.string().required('First Name required').min(2, 'minimum 2 letters'),
-            lastname: Yup.string().required('Last Name required'),
-            password: Yup.string().required('password required').matches(/^(?=.*\d)(?=.*[!@#$%^&*])(?=.*[a-z])(?=.*[A-Z]).{8,}$/,'password must required @ a capital letter and number')
+            // firstname: Yup.string().required('First Name required').min(2, 'minimum 2 letters'),
+            // lastname: Yup.string().required('Last Name required'),
+            // password: Yup.string().required('password required').matches(/^(?=.*\d)(?=.*[!@#$%^&*])(?=.*[a-z])(?=.*[A-Z]).{8,}$/,'password must required @ a capital letter and number')
+            // age: Yup.string().required('age is required').test({
+            //     name:'genderbasedvalidation',
+            //     message: 'your age not accepted',
+            //     test: (value, a) => {
+            //         //value < 18 ? false : true
+            //         if(a.parent.gender === 'male' && value < 21) {
+            //             return false
+            //         }
+            //         if(a.parent.gender === 'female' && value < 18) {
+            //             return false
+            //         }
+            //         return true
+            //     }
+            // })
+            username: Yup.string().required('username required').test({
+                name: 'check user asynchronous',
+                message: 'alreay user exist',
+                test: (async(value, a) => {
+                    let response = await axios.get(`http://jsonplaceholder.typicode.com/users?username=${value}`)
+                    if(response.data.length !==0){
+                        return a.createError({message: 'This user is already exist'})
+                    }else{
+                        return true
+                    }
+                    
+                })
+            })
         }),
         onSubmit:(values)=>{
-            console.log(values)
+            // console.log(values)
             setStudents([...students, values])
         }
     })
   return (
     <div>
         <h1>StudentForm</h1>
-        {console.log(Studentform)}
+        {/* {console.log(Studentform)} */}
         <form onSubmit={Studentform.handleSubmit} className='frm'>
+            <input type='text' name='username' onBlur={Studentform.handleBlur} onChange={Studentform.handleChange} placeholder='username'/>
+            <div>
+                <small style={{color: 'tomato'}}>{Studentform.touched.username && Studentform.errors.username}</small>
+            </div>
+            <br/>
             <input type='text' name='firstname' onBlur={Studentform.handleBlur} onChange={Studentform.handleChange} />
-            <div className='inp'>First Name</div>
             <div>
                 <small style={{color: 'tomato'}}>{Studentform.touched.firstname && Studentform.errors.firstname}</small>
             </div>
@@ -35,6 +69,9 @@ function StudentForm() {
             <div>
                 <small style={{color: 'tomato'}}>{Studentform.touched.lastname && Studentform.errors.lastname}</small>
             </div>
+            <br/>
+            <input type='radio' name='gender' value='male' onBlur={Studentform.handleBlur} onChange={Studentform.handleChange}/>male
+            <input type='radio' name='gender' value='female' onBlur={Studentform.handleBlur} onChange={Studentform.handleChange}/>female
             <br/>
             <input type='text' name='age' onBlur={Studentform.handleBlur} onChange={Studentform.handleChange} placeholder='age'/>
             <div>
@@ -52,10 +89,11 @@ function StudentForm() {
         <ul>
             {
                 students.map((student, i) => {
-                    const {firstname, lastname, age} = student
+                    const {firstname, lastname, age, gender} = student
                     return <li key={i}>
                         <p>{firstname}</p>
                         <p>{lastname}</p>
+                        <i>{gender}</i>
                         <p>{age}</p>
                     </li>
                 })

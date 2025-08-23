004da020c9721579b315fa59316ee09c9b14abf7
BharathSunkarKumar <155315751+BharathSunkaraKumar@users.noreply.github.com>
2025-08-23 10:00:34 +0530

    typescript

--- typescript/index.js
+++ typescript/index.js
@@ -3,6 +3,8 @@ var company = 'sbk';
 var isPublished = false;
 var x = 'bharath';
 console.log(id);
+//data types = strings, number, boolean, any, object, array, tuple
+//special types = void, null, undefined, never
 //arrays
 var ar = ['bharath', 48, true];
 var ar2 = [1, 5, 8];
@@ -38,5 +40,72 @@ console.log('hi');
 //array
 var cities = ['tirupati', 'hyderabad', 'bangalore']; // shrothand notation
 cities[0] = "mangalam";
-var countries = ['india', 'uk', 45687, ["sbk"], { id: 456, name: "bharath" }]; //generic type notation
+//generic type notation
+var countries = ['india', 'uk', 45687, ["sbk"], { id: 456, name: "bharath" }];
 console.log(countries);
+//tuples
+var tuple1 = ["sbk", 48, true];
+// void: That function does not return anything is called void
+var userName = function (a, b) {
+    return a + b;
+};
+console.log(userName(1, 1));
+//null
+var noting;
+//undefined
+var unfind;
+//never
+var dburl = false;
+var failedConnection = function () {
+    throw new Error(("db connetion failed"));
+};
+var dbconnection = function () {
+    try {
+        if (dburl) {
+            console.log("db connected successfully");
+        }
+    }
+    catch (error) {
+        failedConnection();
+    }
+};
+// dbconnection()
+//funcitons
+var addingString = function (a, b) {
+    return a + b;
+};
+// console.log(addingString(1,"sbk"))
+var addingString1 = function (a, b) {
+    return a + b;
+};
+console.log(addingString1(18, 6));
+//optional parameters
+var product = function (title, price, discount) {
+    return "this product is ".concat(title, " and price is ").concat(price);
+};
+console.log(product("redmi k20 pro", 25000, true));
+var redmi = {
+    model: "k20 pro",
+    barnd: "redmi",
+    battery: 4100
+};
+var iphone = {
+    model: "17 pro max",
+    battery: 4500,
+    barnd: "iphone",
+    display: 6.5
+};
+var samsung_Fold = {
+    model: "galaxy z fold 7",
+    barnd: "samsung",
+    battery: 4400,
+    fold_count: 1
+};
+console.log(samsung_Fold);
+var prod = {
+    name: "apple",
+    desc: "An apple is the round, edible fruit of an apple tree.",
+    price: 50,
+    discount: false
+};
+console.log(prod);
--- typescript/index.ts
+++ typescript/index.ts
@@ -4,6 +4,8 @@ let isPublished: boolean = false
 let x: any = 'bharath'
 console.log(id)
 
+//data types = strings, number, boolean, any, object, array, tuple
+//special types = void, null, undefined, never
 //arrays
 
 let ar: any[] = ['bharath', 48, true]
@@ -63,5 +65,113 @@ console.log('hi')
 
 let cities: string[] | number = ['tirupati', 'hyderabad', 'bangalore'] // shrothand notation
 cities[0]= "mangalam"
-let countries: Array<string | number | any> = ['india', 'uk', 45687, ["sbk"], {id: 456, name: "bharath"}] //generic type notation
+
+//generic type notation
+let countries: Array<string | number | any> = ['india', 'uk', 45687, ["sbk"], {id: 456, name: "bharath"}] 
 console.log(countries)
+
+//tuples
+let tuple1: [string, number, boolean] = ["sbk", 48, true]
+
+// void: That function does not return anything is called void
+ 
+const userName = (a:number, b:number) => {
+    return a+b
+}
+console.log(userName(1, 1))
+
+//null
+let noting:null
+
+//undefined
+let unfind:undefined
+
+//never
+
+let dburl: boolean = false
+
+const failedConnection = (): never => {
+     throw new Error(("db connetion failed"));  
+}
+
+const dbconnection = () => {
+    try {
+        if(dburl){
+            console.log("db connected successfully")
+        }
+    } catch (error) {
+        failedConnection()
+    }
+}
+// dbconnection()
+
+//funcitons
+
+const addingString = (a: number, b: string) => {
+    return a+b
+}
+// console.log(addingString(1,"sbk"))
+
+const addingString1 = (a:number, b:number):number => {
+    return a+b
+}
+console.log(addingString1(18,6))
+
+//optional parameters
+
+const product = (title: string, price: number, discount?: boolean) => {
+    return `this product is ${title} and price is ${price}`
+}
+console.log(product("redmi k20 pro", 25000, true))
+
+//interface
+
+interface Mobile {
+    model: string,
+    barnd: string,
+    battery: number,
+    display?: number
+}
+interface FoldMobile extends Mobile {
+    fold_count: number
+}
+const redmi: Mobile = {
+    model: "k20 pro",
+    barnd: "redmi",
+    battery: 4100
+}
+const iphone: Mobile = {
+    model: "17 pro max",
+    battery: 4500,
+    barnd: "iphone",
+    display: 6.5
+}
+const samsung_Fold: FoldMobile = {
+    model: "galaxy z fold 7",
+    barnd: "samsung",
+    battery: 4400,
+    fold_count: 1
+}
+console.log(samsung_Fold)
+
+//type aliases
+
+type datatype = string | number
+
+interface Product {
+    name: datatype,
+    desc: datatype,
+    price: datatype,
+    discount: datatype | boolean
+}
+
+const prod: Product = {
+    name: "apple",
+    desc: "An apple is the round, edible fruit of an apple tree.",
+    price: 50,
+    discount: false
+}
+console.log(prod)
+
+
+

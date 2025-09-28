087dab0a22299efe2630f4805c66a56d6581ad52
BharathSunkarKumar <155315751+BharathSunkaraKumar@users.noreply.github.com>
2025-09-28 12:04:28 +0530

    navbar darkmode

--- react/reactApps/React-Apps1/src/components/Navbar.jsx
+++ react/reactApps/React-Apps1/src/components/Navbar.jsx
@@ -1,15 +1,53 @@
-import React, { useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import './Navbar.css'
 
 const Navbar = () => {
     const[show, setShow] = useState(false)
+    const menuRef = useRef()
+    const btnRef = useRef()
+    const [dark, setDark] = useState(false)
+
+    useEffect(() => {
+        const handleClickOutSide = (e) => {
+            if(menuRef.current && !menuRef.current.contains(e.target) && btnRef.current && !btnRef.current.contains(e.target)) {
+                setShow(false)
+                // console.log(menuRef.current.contains(e.target))
+            }
+        }
+
+        const saveMode = localStorage.getItem('theme')
+        if(saveMode === 'dark') {
+            setDark(true)
+        }
+        document.addEventListener('click', handleClickOutSide)
+
+        // setTimeout((a, b) => {
+        //     console.log(a+b)
+        // }, 0, 1, 2, 13);
+
+        return () => document.removeEventListener('click', handleClickOutSide)
+
+    }, [])
+    const toggleTheme = () => {
+        setDark(!dark)
+        localStorage.setItem('theme', !dark ? "dark" : 'lidht')
+    }
     const showToggle = () => {
         setShow(prev => !prev)
     }
   return (
-    <nav className='nav'>
+    <nav className={`nav ${dark ? 'dark' : ""}`}>
         <div className='logo'>
-            My Nav
+            <div style={{display: 'flex', gap: "10px", alignItems: "center"}}>
+                <div>
+                Nav Bar
+            </div>
+            <div style={{color: 'lightblue', fontSize: '20px'}}>
+                <button onClick={toggleTheme} style={{borderRadius: "30px", padding: "5px", backgroundColor: "purple", color: "white", border: "none", fontSize: "13px", fontWeight: "bold", cursor: "pointer"}}>
+                   {dark ? "Switch to Light Mode" : "Switch to Dark Mode"}
+                </button>
+            </div>
+            </div>
         </div>
         <ul className='nav-link'>
             <li>Home</li>
@@ -19,12 +57,12 @@ const Navbar = () => {
         </ul>
         
         <div className='menu'>
-            <div onClick={showToggle}>
-                menu
-            </div>
+            <button ref={btnRef} onClick={showToggle}>
+                ☰
+            </button>
             {
                 show && (
-                    <ul className='nav-link-mob'>
+                    <ul ref={menuRef} className='nav-link-mob'>
                         <li>Home</li>
                         <li>About</li>
                         <li>Services</li>
@@ -32,9 +70,9 @@ const Navbar = () => {
                     </ul>
                 )
             }
-            <div>
+            {/* <div>
                 dark/lite
-            </div>
+            </div> */}
         </div>
         
     </nav>

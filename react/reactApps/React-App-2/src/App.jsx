c403255b8f5f3132a0eb6436a1faba7227a78e7c
BharathSunkarKumar <155315751+BharathSunkaraKumar@users.noreply.github.com>
2026-01-12 14:31:43 +0530

    api

--- react/reactApps/React-App-2/src/App.jsx
+++ react/reactApps/React-App-2/src/App.jsx
@@ -1,12 +1,13 @@
-import React from 'react'
-import TodoList from '../todoapp/TodoList'
+import React from "react";
+import TodoList from "../todoapp/TodoList";
+import Singup from "./components/Singup";
 
 const App = () => {
   return (
-    <section className='p-3'>
-      <TodoList/>
+    <section className="p-3">
+      <Singup />
     </section>
-  )
-}
+  );
+};
 
-export default App
+export default App;
